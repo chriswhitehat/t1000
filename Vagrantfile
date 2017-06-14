e414eb35cc388e1872978913d155733330a7b0bf
Chris White <white.chris@ghc.org>
2017-06-14 12:41:52 -0700

    initial commit

---
+++ Vagrantfile
@@ -0,0 +1,63 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure("2") do |config|
+  # The most common configuration options are documented and commented below.
+  # For a complete reference, please see the online documentation at
+  # https://docs.vagrantup.com.
+
+  # Every Vagrant development environment requires a box. You can search for
+  # boxes at https://atlas.hashicorp.com/search.
+
+  config.proxy.http     = "http://proxy.ghc.org:8080"
+  config.proxy.https    = "http://proxy.ghc.org:8080"
+  config.proxy.no_proxy = "localhost,127.0.0.1,ghc.org"
+
+  config.butcher.verify_ssl = false
+
+  config.vm.define "opencanary01" do |opencanary|
+    opencanary.vm.box = "ubuntu/trusty64"
+    opencanary.vm.hostname = "opencanary"
+    opencanary.vm.network "private_network", ip: "192.168.50.10", virtualbox__intnet: "white_network"
+    opencanary.vm.network :forwarded_port, guest: 22, host: 22000, id: 'ssh'
+    config.vm.provider :virtualbox do |vb|
+      vb.linked_clone = true
+      vb.customize ["modifyvm", :id, "--memory", "1024"]
+      vb.customize ['modifyvm', :id, '--natnet1', '192.168.222.0/24']
+      vb.customize ['modifyvm', :id, "--natdnshostresolver1", "off"]
+    end
+
+    opencanary.vm.provision "chef_client" do |client|
+      client.chef_server_url = "https://esa-ir-dca.ghc.org/organizations/ghc"
+      client.validation_client_name = "ghc-validator"
+      client.validation_key_path = "../../.chef/ghc-validator.pem"
+      client.add_role "base"
+      client.add_recipe "t1000"
+    end
+  end
+
+  config.vm.define "opencanary02" do |opencanary|
+    opencanary.vm.box = "ubuntu/trusty64"
+    opencanary.vm.hostname = "opencanary"
+    opencanary.vm.network "private_network", ip: "192.168.50.11", virtualbox__intnet: "white_network"
+    opencanary.vm.network :forwarded_port, guest: 22, host: 22000, id: 'ssh'
+    config.vm.provider :virtualbox do |vb|
+      vb.linked_clone = true
+      vb.customize ["modifyvm", :id, "--memory", "1024"]
+      vb.customize ['modifyvm', :id, '--natnet1', '192.168.222.0/24']
+      vb.customize ['modifyvm', :id, "--natdnshostresolver1", "off"]
+    end
+
+    opencanary.vm.provision "chef_client" do |client|
+      client.chef_server_url = "https://esa-ir-dca.ghc.org/organizations/ghc"
+      client.validation_client_name = "ghc-validator"
+      client.validation_key_path = "../../.chef/ghc-validator.pem"
+      client.add_role "base"
+      client.add_recipe "t1000"
+    end
+  end
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,116 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+user 'canary' do
+  action :create
+  comment 'Canary User'
+  uid 1000
+  gid 'users'
+  home '/home/random'
+  shell '/bin/bash'
+  password '$1$JJsvHslV$szsCjVEroftprNn4JHtDi.'
+  supports :manage_home => true
+end
+
+
+package 'install_hwe' do
+  package_name ['linux-image-generic-lts-xenial', 'linux-generic-lts-xenial']
+  action :install
+  notifies :reboot_now, 'reboot[hwe_upgraded]', :immediately
+end
+
+directory '/etc/opencanary' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
+package 'install_opencanary_deps' do
+  package_name ['python-dev', 'python-pip', 'python-pcapy', 'build-essential', 'libssl-dev', 'libffi-dev', 'samba' ]
+  action :install
+end
+
+
+execute 'pip_opencanary_deps' do
+  command 'pip install scapy; pip install rdpy, pip install opencanary'
+  creates '/tmp/something'
+  action :run
+end
+
+
+template '/etc/smb/smb.conf' do
+  source 'smb/smb.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+
+template '/etc/syslog-ng/conf.d/smb.conf' do
+  source 'source.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+
+
+####################
+# Create json lists
+####################
+http_skins = ""
+node[:t1000][:opencanary][:http][:skins].each do |name, skin|
+	http_skins << "{\n"
+	http_skins << "            \"name\": \"#{skin[:name]}\",\n"
+	http_skins << "            \"desc\": \"#{skin[:desc]}\"\n"
+	http_skins << "        },\n"
+end
+http_skins.chomp(",")
+
+
+httpproxy_skins = ""
+node[:t1000][:opencanary][:httpproxy][:skins].each do |name, skin|
+	httpproxy_skins << "{\n"
+	httpproxy_skins << "            \"name\": \"#{skin[:name]}\",\n"
+	httpproxy_skins << "            \"desc\": \"#{skin[:desc]}\"\n"
+	httpproxy_skins << "        },\n"
+end
+httpproxy_skins.chomp(",")
+
+
+smb_files = ""
+node[:t1000][:opencanary][:httpproxy][:skins].each do |name, smb_file|
+	smb_files << "{\n"
+	smb_files << "            \"name\": \"#{smb_file[:name]}\",\n"
+	smb_files << "            \"type\": \"#{smb_file[:type]}\"\n"
+	smb_files << "        },\n"
+end
+smb_files.chomp(",")
+
+
+telnet_honeycreds = ""
+node[:t1000][:opencanary][:httpproxy][:skins].each do |name, telnet_honeycred|
+	telnet_honeycreds << "{\n"
+	telnet_honeycreds << "            \"username\": \"#{telnet_honeycred[:username]}\",\n"
+	telnet_honeycreds << "            \"password\": \"#{telnet_honeycred[:password]}\"\n"
+	telnet_honeycreds << "        },\n"
+end
+telnet_honeycreds.chomp(",")
+
+
+template '/etc/opencanary/opencanary.conf' do
+  source 'opencanary/opencanary.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables({
+  	:http_skins => http_skins,
+  	:httpproxy_skins => httpproxy_skins,
+  	:smb_files => smb_files,
+  	:telnet_honeycreds => telnet_honeycreds
+  	})
+end
