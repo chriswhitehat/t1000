a31f2b884ab44df266b80e6ce2350f4abf61bb2e
Chris White <white.chris@ghc.org>
2018-01-08 16:03:12 -0800

    missing dependency on t1000 cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,8 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures t1000'
 long_description 'Installs/Configures t1000'
-version '0.2.3'
+version '0.2.4'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-depends 't1000'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
