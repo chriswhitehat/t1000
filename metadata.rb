a0ef876e3c1025f30a68c73f0f793cfba063bae1
Chris White <chriswhite@cmwftw.local>
2018-09-30 14:30:32 -0700

    b

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures t1000'
 long_description 'Installs/Configures t1000'
 
-version '0.5.3'
+version '0.5.4'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
