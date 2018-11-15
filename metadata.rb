f1df02f511f67e42dfc502562621552d0d776c78
Chris White <chriswhite@cmwftw.kpca.l3sc.kp.org>
2018-11-15 11:25:00 -0800

    added broadcasts and jittered respondered cron

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures t1000'
 long_description 'Installs/Configures t1000'
 
-version '0.12.1'
+version '0.12.2'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
