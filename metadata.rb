71492a6fb40be91c16be8f25fb8afd6c13ae53ea
Chris White <chriswhite@cmwftw.kpca.l3sc.kp.org>
2018-11-15 13:33:10 -0800

    bug in cron, missing full path

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures t1000'
 long_description 'Installs/Configures t1000'
 
-version '0.12.3'
+version '0.12.4'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
