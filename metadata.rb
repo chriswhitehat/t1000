e69ecc5b4dcad55efe9eac792ce312eba1fbadcd
Chris White <chriswhite@cmwftw.local>
2018-12-11 21:49:41 -0800

    additional interfaces changes

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures t1000'
 long_description 'Installs/Configures t1000'
 
-version '0.13.0'
+version '0.13.1'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
