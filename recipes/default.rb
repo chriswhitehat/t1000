d6110bc998c8f3f656ba07473e6e84c02260c2ac
Chris White <chriswhite@cmwftw.local>
2018-12-14 14:12:41 -0800

    removed etc hosts management in favor of hostname resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,12 +13,12 @@ if node[:t1000][:mgmt][:configure]
     mode '0644'
   end
 
-  template '/etc/hosts' do
-    source 'hosts.erb'
-    owner 'root'
-    group 'root'
-    mode '0644'
-  end
+  # template '/etc/hosts' do
+  #   source 'hosts.erb'
+  #   owner 'root'
+  #   group 'root'
+  #   mode '0644'
+  # end
   
 end
 
